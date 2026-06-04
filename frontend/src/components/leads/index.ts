dc4e8a8069b047c1f769e725aad2b596d1c97aeb
ItsKevinRafaell <itskevin.rafael@gmail.com>
2026-06-04 12:52:46 +0700

    feat(leads): update barrel export with all extracted modals

--- frontend/src/components/leads/index.ts
+++ frontend/src/components/leads/index.ts
@@ -2,3 +2,7 @@
 
 export { default as LeadsFilterBar } from "./LeadsFilterBar";
 export { default as LeadsSearchActions } from "./LeadsSearchActions";
+export { default as SalesModal } from "./SalesModal";
+export { default as FollowUpModal } from "./FollowUpModal";
+export { default as WaPreviewModal } from "./WaPreviewModal";
+export { default as BlastModal } from "./BlastModal";
